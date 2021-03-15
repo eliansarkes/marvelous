9ea69f66c267255ade7b237c213e9afb1f3fbfcb
eliansarkes <77165132+eliansarkes@users.noreply.github.com>
2021-03-14 22:07:26 -0600

    Add files via upload

---
+++ handler/paypal.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<?php
+
+session_start();
+
+?>
+<html lang="en">
+
+<head>
+    <!-- Add meta tags for mobile and IE -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title> PayPal Smart Payment Buttons Integration | Client Demo </title>
+</head>
+
+<body>
+    <!-- Set up a container element for the button -->
+    <div id="paypal-button-container"></div>
+
+    <!-- Include the PayPal JavaScript SDK -->
+    <script src="https://www.paypal.com/sdk/js?client-id=Ae0WJIcwhqZzENj57rCyt3bW7ZTSuYf2Bnje-o8LEnTtmSK02qhoydr4sdkHGwO7k4Bt_Jikqa3M8Fhw&currency=CAD"></script>
+
+    <script>
+        // Render the PayPal button into #paypal-button-container
+        paypal.Buttons({
+
+            // Set up the transaction
+            createOrder: function(data, actions) {
+                return actions.order.create({
+                    purchase_units: [{
+                        amount: {
+                            value: '<?php echo $_SESSION['total']* ['quantity'] ?>'
+                        }
+                    }]
+                });
+            },
+
+            // Finalize the transaction
+            onApprove: function(data, actions) {
+                return actions.order.capture().then(function(details) {
+                    // Show a success message to the buyer
+                    alert('Transaction completed by ' + details.payer.name.given_name + '!');
+                });
+            }
+
+
+        }).render('#paypal-button-container');
+    </script>
+</body>
+
+</html>
+    
